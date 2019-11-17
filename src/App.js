c956403e10f998c23ac341cdc6ff4eafd317f89b
Jonatas Pinto <jonatastdb_jp@hotmail.com>
2019-11-16 23:24:45 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Home from './containers/Home/Home.js';
+
+function App() {
+  return (
+    <>
+    <Home/>
+    </>
+  )
+}
+export default App;
+
---
+++ src/pt.js
@@ -0,0 +1,3 @@
+export const Lang ={
+    welcome: "Bem vindo ao conforto animal"
+}
