ac63bdac0c042151e3e86f44894648d35b135e42
Alan Araujo <alan.techsafe@hotmail.com>
2019-11-25 01:36:19 -0300

    Updates

---
+++ precache-manifest.c16dab6eb3b11619d06f082003f1fe23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74c209857988b6d87d9609c545a05142",
+    "url": "/conforto-animal/index.html"
+  },
+  {
+    "revision": "200da03ef99309357042",
+    "url": "/conforto-animal/static/css/main.25e3c742.chunk.css"
+  },
+  {
+    "revision": "80ad6166b3979548a475",
+    "url": "/conforto-animal/static/js/2.e1aec6ec.chunk.js"
+  },
+  {
+    "revision": "200da03ef99309357042",
+    "url": "/conforto-animal/static/js/main.8cb8b1c4.chunk.js"
+  },
+  {
+    "revision": "1f32951fa515bfb4763e",
+    "url": "/conforto-animal/static/js/runtime-main.8d5c5aa5.js"
+  },
+  {
+    "revision": "d1e24f32537836a446175b16a5a330df",
+    "url": "/conforto-animal/static/media/logo.d1e24f32.png"
+  }
+]);
