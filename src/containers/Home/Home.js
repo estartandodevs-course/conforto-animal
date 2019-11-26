cd770e13d887112e73717aaa4dd26bd2b9e6e1e0
Jonatas Pinto <jonatastdb_jp@hotmail.com>
2019-11-26 12:34:33 -0300

    implements splash in home

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react'
 import MapContainer from '../../components/MapContainer';
+import SplashContainer from '../SplashScreen/Splash'
 import {HomeContainer} from './HomeStyle'
 import Header from '../../components/Header/Header'
 
 export default class Home extends Component {
+
+  state={
+    splash: true
+  }
+
+  componentDidMount(){
+    setTimeout(()=>{this.setState({splash: false})},2000)
+  }
   render() {
+    const {splash} = this.state
     return(
+      splash ? <SplashContainer/> 
+      :
       <HomeContainer flexDirection={'column'}>
         <Header/>
         <MapContainer/>
       </HomeContainer>
     )
+      
   }
 }
