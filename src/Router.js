fb008603f85b40bfd847d3497fd464ec40e11f2c
Gaby <lubaaenzin@gmail.com>
2019-11-26 19:38:03 -0200

    LoginScreen

--- src/Router.js
+++ src/Router.js
@@ -6,11 +6,13 @@ import {
   } from "react-router-dom";
 import Home from './containers/Home/Home.js';
 import SplashScreen  from './containers/SplashScreen/Splash.js';
+import Login from './containers/Login/Login.js';
 
 export default function Routers() {
   return (
     <BrowserRouter>
       <Switch>
+      <Route path="/login" component={Login}/>
         <Route path="/" exact component={Home}/>
         <Route path="/spl" component={SplashScreen}/>
       </Switch>
