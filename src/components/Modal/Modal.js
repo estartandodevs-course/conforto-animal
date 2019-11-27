47619e2545ada585b5acac1c4f97f30736b9c7da
Gaby <lubaaenzin@gmail.com>
2019-11-27 00:54:16 -0200

    editModal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import { ModalContainer } from './ModalStyle.js';
+import back from '../../assets/images/back.png'
+import gato from '../../assets/images/catIMG.png'
+import cachorro from '../../assets/images/dogIMG.png'
 
 export default function Modal(props) {
   return (
     <ModalContainer style={{left: props.show ? 0 : "100vw"}}>
-      <p id='close' onClick={props.close}>X</p>
-      {props.children}
+      <img src={back} id='close' onClick={props.close}/>
+      <div id="Title">
+        <h1>{props.title}</h1>
+      </div>
+      <div className='Icones'>
+        <div>
+          <img src={cachorro} />
+          <h1>Doguíneo</h1>
+        </div>
+        <div>
+          <img src={gato} />
+          <h1>Gatíneo</h1>
+        </div>
+      </div>
+      <h2>{props.description1}</h2>
+      <h3>{props.description2}</h3>
+      <h3>{props.description3}</h3>
     </ModalContainer>
   );
 }
