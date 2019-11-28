a4d1f63ebf8a31c095e1eaf4dec6144952f8c91b
Danyel <danyelsilva320@gmail.com>
2019-11-28 15:27:22 -0300

    fix button

--- src/components/Button/ButtonStyle.js
+++ src/components/Button/ButtonStyle.js
@@ -8,7 +8,10 @@ export const ButtonContainer = styled.button`
     border-radius: 10px;
     background-color: #01A58D;
     color: #fff;
+<<<<<<< develop
     cursor: pointer;
+=======
+>>>>>>> fix button
     
     &:focus{
         outline:none;
