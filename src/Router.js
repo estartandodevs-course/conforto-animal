510a606681c4211358a8cab217dc18584993ecff
Danyel <danyelsilva320@gmail.com>
2019-11-30 09:57:43 -0300

    create adoption container

--- src/Router.js
+++ src/Router.js
@@ -7,6 +7,7 @@ import {
 import Home from './containers/Home/Home.js';
 import SplashScreen  from './containers/SplashScreen/Splash.js';
 import Login from './containers/Login/Login.js';
+import AdoptionContainer from './containers/Adoption/Adoption'
 import DonateContainer from './containers/Donate/Donate'
 import Header from './components/Header/Header'
 
@@ -20,6 +21,7 @@ export default function Routers() {
         <Route path="/" exact component={Home}/>
         <Route path="/home" component={Home}/>
         <Route path="/spl" component={SplashScreen}/>
+        <Route path="/adoption" component={AdoptionContainer}/>
         <Route path="/donate" component={DonateContainer}/>
       </Switch>
     </BrowserRouter>
---
+++ src/containers/Adoption/AdoptionStyle.js
@@ -0,0 +1,39 @@
+import styled from 'styled-components'
+import '../../assets/fonts/fonts.css';
+import { FlexContainer } from '../../shared/Structure';
+
+
+export const AdoptionContainer = styled(FlexContainer)`
+  display: flex;
+    flex-direction: column;
+    width: 100vw;
+    height: 100vh;
+    background-color: #01A58D;
+`
+export const FormPet = styled.form`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    width: 100vw;
+    height: 50vh;
+    background-color: #fff;
+    @media screen and (min-width: 768px){
+        width: 50vw;
+    }
+    .label{
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        @media screen and (min-width: 768px){
+            display: flex;
+            justify-content: flex-start;
+            align-items: center;
+        }
+    }
+    .description{
+        display: flex; 
+        flex-direction: column;
+        justify-content: space-around;
+        align-items: flex-start;
+    }
+`
