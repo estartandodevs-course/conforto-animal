9494ec6648a646650a29ac5711ecae7d59916d00
Ana Kamille <anakamille90@gmail.com>
2019-11-30 21:36:08 -0300

    WIP

--- src/containers/ChatScreen/chatscreenstyle.js
+++ src/containers/ChatScreen/chatscreenstyle.js
@@ -1,16 +1,12 @@
 import styled from 'styled-components'
 import { FlexContainer } from '../../shared/Structure'
-import cachorro from '../../assets/images/cachorroBack.png'
-import gato from '../../assets/images/GatoBack.png'
+import fundo from '../../assets/images/fundo.png'
 
 
 export const HomeAside = styled(FlexContainer)`
 background:#107768;
 min-height:100vh;
-background-image: url(${cachorro})50% ,url(${gato})50%;
-background-position:
-    top right, /* this positions the first image */
-    bottom left; /* this positions the second image */
-  background-repeat:
-    repeat; 
+background-image: url(${fundo});
+
+   
 `
