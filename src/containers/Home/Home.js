27a3b2f4ec0bba7e371e414b839ec63429a578f5
Gaby <lubaaenzin@gmail.com>
2019-12-03 19:42:45 -0200

    fix errors

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -2,19 +2,10 @@ import React, { Component } from 'react'
 import MapContainer from '../../components/MapContainer';
 import SplashContainer from '../SplashScreen/Splash'
 import {HomeContainer, HomeAside} from './HomeStyle'
-// import Button from '../../components/Button/Button';
-<<<<<<< HEAD
-// import Button from '../../components/Button/Button'
 import Card from '../../components/Card/Card';
-
-=======
-import Button from '../../components/Button/Button'
-import SliderComponent from '../../components/SliderComponent/SliderComponent'
->>>>>>> SliderComponent
 export default class Home extends Component {
   
   state={
-<<<<<<< HEAD
     splash: (window.location.pathname ==="/" ? true : false),
     pets: [
       {
@@ -28,40 +19,20 @@ export default class Home extends Component {
         title:"jaja",
         sexo:"masculino",
         age:"2 meses"
-      }
-    ]
-=======
-    content:[],
+      },
+    ],
     splash: (window.location.pathname ==="/" ? true : false)
->>>>>>> SliderComponent
   }
   
   componentDidMount(){
     setTimeout(()=>{this.setState({splash: false})},2000);
-    this.loadAnimals()   
-  }
-  loadAnimals =()=>{
-    fetch("http://s3-sa-east-1.amazonaws.com/zee.dog/athena/breedSizes.json").then(response => response.json()).then(res => this.setState({
-      content:res
-    })
-     ).catch(err => alert("deu erro"))
-  }
-  logout=()=>{
-    localStorage.clear();
-    window.location.href = '/login';
   }
   render() {
-<<<<<<< HEAD
     const {splash, pets} = this.state
-    
-=======
-    const {splash,logout,content} = this.state
->>>>>>> SliderComponent
+
     return(
       splash ? <SplashContainer/> 
-      :<div>
-      <HomeContainer flexDirection={'column'}>
-<<<<<<< HEAD
+      :<HomeContainer flexDirection={'column'}>
         <MapContainer/>
         <HomeAside flexDirection={'column'} alignItems={'center'}>
           {pets.map((pet, index)=> {
@@ -76,14 +47,7 @@ export default class Home extends Component {
             )})
           }
         </HomeAside>
-=======
-        <Header/>)
-        <MapContainer/>
-        <Button onClick={logout}>Fazer Logout</Button>
->>>>>>> SliderComponent
       </HomeContainer>
-      <SliderComponent content={this.state.content}/>
-      </div>
     )
   }
 }
