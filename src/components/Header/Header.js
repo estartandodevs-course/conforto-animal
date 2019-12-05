5b4cb5f5e1ebdac4a30a1cf07a54d2ce3d6f6dc1
Gaby <lubaaenzin@gmail.com>
2019-12-05 02:03:57 -0200

    Update menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import { HeaderContainer, MenuContainer, ProfileImg, ListMenu, MenuDesktop} from './HeaderStyle'
+import { HeaderContainer, MenuContainer, ProfileImg,Profilename, ListMenu, MenuDesktop} from './HeaderStyle'
 import logo from '../../assets/images/logo.png'
 import Search from '../../assets/images/search.png'
 import Menu from '../../assets/images/menu 1.png'
@@ -68,23 +68,12 @@ const Header = (props) => {
         </MenuContainer>
         <MenuDesktop>
             <ul>
-                <Link to="/">
-                    <li>HOME</li>
-                </Link>
-                <Link to="/adoption">
-                    <li>ADOTE</li>
-                </Link>
-                <Link to="/donate">
-                    <li>DOE</li>
-                </Link>
-                <Link to="/">
-                    <li>QUEM <br></br>SOMOS</li>
-                </Link>
-                <Link to="/">
-                    <li className="last-border">MEU <br></br> PERFIL</li>
-                </Link>
-
-            </ul>
+                {optionsMenu.map((options, index) =>{
+                    return <Link to={options.route} key={index}>
+                        <li>{options.text}</li>
+                    </Link>
+                })}
+            </ul>           
         </MenuDesktop>
        </HeaderContainer>
        
