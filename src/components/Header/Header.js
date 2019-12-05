1fb2c278386a05197ed6fd62be11c68c7e338177
Gaby <lubaaenzin@gmail.com>
2019-12-05 01:50:30 -0200

    wip

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -35,7 +35,7 @@ const Header = (props) => {
        <HeaderContainer>
            <img className="menuHeader" src={Menu} alt="" onClick={()=> setShowMenu(!showMenu)}/>
             <img className="logoImg" alt="" src={logo}/>
-            <img className="chatHeader" alt="" onClick={props.onClick} src={Search}/>
+            <Link to='/chat'><img className="chatHeader" alt="Chat" src={Search}/></Link>
             
         <MenuContainer show={showMenu} onClick={()=>setShowMenu(!showMenu)}>
             <p id="close" onClick={()=> setShowMenu(!showMenu)}>X</p>
