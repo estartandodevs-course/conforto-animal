45432635a640be836e6b6f037351322d1f1ec361
Alan Araujo <alan.techsafe@hotmail.com>
2019-12-08 21:58:53 -0300

    Implement service with rxjs + improvement map component

---
+++ src/services/pets/index.js
@@ -0,0 +1,34 @@
+import { BehaviorSubject } from "rxjs";
+import { firebase } from "../../firebase";
+
+export class PetService {
+  Dogs = new BehaviorSubject([]);
+  Cats = new BehaviorSubject([]);
+
+  constructor() {
+    this.getDogs();
+    this.getCats();
+  }
+
+  getDogs = () => {
+    firebase
+      .database()
+      .ref("pets/dog")
+      .on("value", snapshot => {
+        let res = snapshot.val();
+        let dogs = Object.keys(res).map(key => res[key]);
+        this.Dogs.next(dogs);
+      });
+  };
+
+  getCats = async () => {
+    await firebase
+      .database()
+      .ref("pets/cat")
+      .on("value", snapshot => {
+        let res = snapshot.val();
+        let cats = Object.keys(res).map(key => res[key]);
+        this.Cats.next(cats);
+      });
+  };
+}
