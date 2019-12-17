ac7ee096a67e2d94def8022a9bd40e769744f770
Gaby <lubaaenzin@gmail.com>
2019-12-17 01:07:14 -0200

    Convert donation to function

--- src/containers/Donate/Donate.js
+++ src/containers/Donate/Donate.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { DonateContainer, UploadImg, FormPets } from "./DonateStyle";
 import Input from "../../components/Input/Input";
 import paw from "../../assets/images/paw.png";
@@ -11,57 +11,56 @@ import Select from "../../components/select/Select";
 import { PetService } from "../../services/pets";
 import Steps from "../../components/Steps/Steps";
 
-export default class Donate extends Component {
-  state = {
-    showModal: true,
-    image: [],
-    pet: new Pet(),
-    class: "",
-    url: "",
-    progress: 0
+const Donate=()=> {
+
+  const [showModal, setShowModal]=useState(true);
+  const [image, setImage]=useState([]);
+  const [pet, setPet]=useState(new Pet());
+  const [Class, setClass]=useState("");
+  const [url, setUrl]=useState("");
+  const [progress, setProgress]=useState(0);
+
+  const petService = new PetService();
+
+  useEffect(() => {
+    getLocation();
+  });
+
+  const toggleModal = () => {
+    setShowModal(!showModal);
   };
 
-  petService = new PetService();
-
-  componentDidMount() {
-    this.getLocation();
-  }
-
-  toggleModal = () => {
-    this.setState({ showModal: !this.state.showModal });
+  const isCat = async () => {
+    await setClass("cat" );
+    toggleModal();
   };
 
-  isCat = async () => {
-    await this.setState({ class: "cat" });
-    this.toggleModal();
+  const isDog = async () => {
+    await setClass("dog");
+    toggleModal();
   };
 
-  isDog = async () => {
-    await this.setState({ class: "dog" });
-    this.toggleModal();
-  };
-
-  getLocation = () => {
+  const getLocation = () => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(position => {
-        const pet = Object.assign({}, this.state.pet);
-        pet.location.lat = position.coords.latitude;
-        pet.location.lng = position.coords.longitude;
-        this.setState({ pet: pet });
+        const pet_ = Object.assign({}, pet);
+        pet_.location.lat = position.coords.latitude;
+        pet_.location.lng = position.coords.longitude;
+        setPet(pet_);
       });
     } else {
       console.log("Geolocation is not supported by this browser.");
     }
   };
 
-  onChange = event => {
-    const pet = Object.assign({}, this.state.pet);
+  const onChange = event => {
+    const pet_ = Object.assign({}, pet);
     const input = event.target.name;
-    pet[input] = event.target.value;
-    this.setState({ pet: pet });
+    pet_[input] = event.target.value;
+    setPet(pet_);
   };
 
-  handleUpload = e => {
+  const handleUpload = e => {
     if (e.target.files[0]) {
       const image = e.target.files[0];
       const uploadTask = firebase
@@ -75,7 +74,7 @@ export default class Donate extends Component {
           const progress = Math.round(
             (snapshot.bytesTransferred / snapshot.totalBytes) * 100
           );
-          this.setState({ progress });
+          setProgress(progress);
         },
         error => {
           // Error function ...
@@ -89,149 +88,147 @@ export default class Donate extends Component {
             .child(image.name)
             .getDownloadURL()
             .then(url => {
-              const pet = Object.assign({}, this.state.pet);
-              pet.imgSrc = url;
-              this.setState({ pet: pet });
+              const pet_ = Object.assign({}, pet);
+              pet_.imgSrc = url;
+              setPet( pet_ );
             });
         }
       );
     }
   };
 
-  onSubmit = async event => {
-    const response = await this.petService.insertPet(
-      this.state.pet,
-      this.state.class
+  const onSubmit = async event => {
+    const response = await petService.insertPet(
+      pet,
+      Class
     );
     console.log("REF PETS :: ", response);
-    this.setState({ pet: new Pet() });
+     setPet(new Pet());
   };
 
-  render() {
-    const { pet, showModal, url, progress } = this.state;
-    return (
-      <DonateContainer alignItems={"center"} flexDirection={"column"}>
-        <FormPets>
-          <UploadImg>
-            {progress < 100 ? <label htmlFor="avatar">+</label> : <p>ok</p>}
-            <input
-              type="file"
-              id="avatar"
-              accept="image/png, image/jpeg"
-              multiple
-              style={{ display: "none" }}
-              name={"imgSrc"}
-              value={url}
-              onChange={this.handleUpload}
-            />
-          </UploadImg>
-
-          <div className="label">
-            <img src={paw} alt="" />
-            <Input
-              name={"name"}
-              placeholder="Nome"
-              value={pet.name}
-              action={this.onChange}
-              type={"text"}
-            />
-          </div>
-
-          <div className="label">
-            <img src={paw} alt="" />
-            <Select
-              name={"sexo"}
-              placeholder="Sexo"
-              value={pet.sexo}
-              action={this.onChange}
-              type={"select"}
-              options={[
-                { name: "Feminino", value: "feminino" },
-                { name: "Masculino", value: "masculino" }
-              ]}
-            />
-          </div>
-
-          <div className="label">
-            <img src={paw} alt="" />
-            <Input
-              name={"breed"}
-              placeholder="Raça"
-              value={pet.breed}
-              action={this.onChange}
-              type={"text"}
-            />
-          </div>
-
-          <div className="label">
-            <img src={paw} alt="" />
-            <Input
-              name={"age"}
-              placeholder="Idade"
-              value={pet.age}
-              action={this.onChange}
-              type={"number"}
-            />
-          </div>
-
-          <div className="label">
-            <img src={paw} alt="" />
-            <Select
-              name={"castrated"}
-              value={pet.castrated}
-              placeholder="Castrado"
-              action={this.onChange}
-              type={"select"}
-              options={[
-                { name: "Sim", value: "Sim" },
-                { name: "Não", value: "Não" }
-              ]}
-            />
-          </div>
-
-          <div className="label">
-            <img src={paw} alt="" />
-            <Select
-              name={"dewormed"}
-              placeholder="Vermifugado"
-              value={pet.dewormed}
-              action={this.onChange}
-              type={"select"}
-              options={[
-                { name: "Sim", value: "Sim" },
-                { name: "Não", value: "Não" }
-              ]}
-            />
-          </div>
-
-          <div className="description">
-            <p>{Lang.description}</p>
-            <Input
-              placeholder="Descrição"
-              name={"description"}
-              value={pet.description}
-              action={this.onChange}
-              type={"textarea"}
-            ></Input>
-          </div>
-
-          <Button
-            action={this.onSubmit}
-            className="btn-bottom"
-            value={"send"}
+  return (
+    <DonateContainer alignItems={"center"} flexDirection={"column"}>
+      <FormPets>
+        <UploadImg>
+          {progress < 100 ? <label htmlFor="avatar">+</label> : <p>ok</p>}
+          <input
+            type="file"
+            id="avatar"
+            accept="image/png, image/jpeg"
+            multiple
+            style={{ display: "none" }}
+            name={"imgSrc"}
+            value={url}
+            onChange={handleUpload}
           />
-        </FormPets>
+        </UploadImg>
 
-        <Modal
-          show={showModal}
-          child={"donate"}
-          title={Lang.donateTitle}
-          setDog={this.isDog}
-          setCat={this.isCat}
+        <div className="label">
+          <img src={paw} alt="" />
+          <Input
+            name={"name"}
+            placeholder="Nome"
+            value={pet.name}
+            action={onChange}
+            type={"text"}
+          />
+        </div>
+
+        <div className="label">
+          <img src={paw} alt="" />
+          <Select
+            name={"sexo"}
+            placeholder="Sexo"
+            value={pet.sexo}
+            action={onChange}
+            type={"select"}
+            options={[
+              { name: "Feminino", value: "feminino" },
+              { name: "Masculino", value: "masculino" }
+            ]}
+          />
+        </div>
+
+        <div className="label">
+          <img src={paw} alt="" />
+          <Input
+            name={"breed"}
+            placeholder="Raça"
+            value={pet.breed}
+            action={onChange}
+            type={"text"}
+          />
+        </div>
+
+        <div className="label">
+          <img src={paw} alt="" />
+          <Input
+            name={"age"}
+            placeholder="Idade"
+            value={pet.age}
+            action={onChange}
+            type={"number"}
+          />
+        </div>
+
+        <div className="label">
+          <img src={paw} alt="" />
+          <Select
+            name={"castrated"}
+            value={pet.castrated}
+            placeholder="Castrado"
+            action={onChange}
+            type={"select"}
+            options={[
+              { name: "Sim", value: "Sim" },
+              { name: "Não", value: "Não" }
+            ]}
+          />
+        </div>
+
+        <div className="label">
+          <img src={paw} alt="" />
+          <Select
+            name={"dewormed"}
+            placeholder="Vermifugado"
+            value={pet.dewormed}
+            action={onChange}
+            type={"select"}
+            options={[
+              { name: "Sim", value: "Sim" },
+              { name: "Não", value: "Não" }
+            ]}
+          />
+        </div>
+
+        <div className="description">
+          <p>{Lang.description}</p>
+          <Input
+            placeholder="Descrição"
+            name={"description"}
+            value={pet.description}
+            action={onChange}
+            type={"textarea"}
+          ></Input>
+        </div>
+
+        <Button
+          action={onSubmit}
+          className="btn-bottom"
+          value={"send"}
         />
-        
-        <Steps/>
-      </DonateContainer>
+      </FormPets>
+
+      <Modal
+        show={showModal}
+        child={"donate"}
+        title={Lang.donateTitle}
+        setDog={isDog}
+        setCat={isCat}
+      />
+
+      <Steps/>
+    </DonateContainer>
     );
-  }
 }
+export default Donate
